08d6298f2130c70f15c176b221812f9ee320d03d
Anova Hacks <anovahacks51@Anovas-MacBook-Pro.local>
2019-03-16 14:44:46 -0700

    update

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-document.getElementById('score').innerText = ('Score: ' + value);
-
 function goBack() {
     window.history.back();
   }
-
-function randomize(){
-    var num = Math.random();
-    if(num < 0.5){
-        document.getElementById();
-    }
-    
-}

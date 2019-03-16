bb6eeed11eef417e29bd2a7b5d8a4651092c561e
Anova Hacks <anovahacks51@Anovas-MacBook-Pro.local>
2019-03-16 09:12:21 -0700

    update

---
+++ test1.js
@@ -0,0 +1,13 @@
+var quizC = document.getElementById('questions');
+var resultsC = document.getElementById('answers');
+var submitB = document.getElementById('results');
+
+function createQuiz(){
+
+}
+
+function showResults(){
+
+}
+
+submitB.addEventListener('click', showResults);

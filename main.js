6ca3adbd9c4e0e79025f368bcf99ff4183c73d95
Anova Hacks <anovahacks51@Anovas-MacBook-Pro.local>
2019-03-16 11:50:44 -0700

    update

--- main.js
+++ main.js
@@ -2,27 +2,6 @@ var value = 0;
 
 document.getElementById('score').innerText = ('Score: ' + value);
 
-
-var myQuestions = [
-    {
-        question: "What is Chlorophyll?",
-        answers: {
-            A: 'Green pigment in plants that absorbs light energy used to carry out photosynthesis',
-            B: 'Chlorine Cells',
-            C: 'The process by which cells obtain energy from glucose',
-            D: 'Growth of a plant away from light'
-        },
-        correctAnswer: "A"
-    },
-    {
-        question: "What is photosynthesis?",
-        answers: {
-            A: 'Green PIgment in plants which absorbs light energy used to create sugars and energy',
-            B: 'Process by which plants and some other organisms use light energy to covert water and carbon dioxide into glucose(sugars) and oxygen',
-            C: 'The process by which plants release vapor into the air through stomata',
-            D: 'When the organism’s growth stops'
-        },
-        correctAnswer: "B"
-    }
-]
-
+function goBack() {
+    window.history.back();
+  }

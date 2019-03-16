24d7cb31839361d4b72729757f034752984881c2
Anova Hacks <anovahacks51@Anovas-MacBook-Pro.local>
2019-03-16 09:29:21 -0700

    update

--- test1.js
+++ test1.js
@@ -1,6 +1,32 @@
 var quizC = document.getElementById('questions');
-var resultsC = document.getElementById('answers');
+var resultsC = document.getElementById('answer');
 var submitB = document.getElementById('results');
+
+var myQuestions = [
+    {
+        question: "Which Planet is the Biggest?",
+        answers: {
+            A: "Earth",
+            B: "Jupiter",
+            C: "Venus",
+            D: "Saturn"
+        },
+        correctAnswer: "B"
+    },
+    {
+        question: "Which Planet is the Coldest",
+        answers: {
+            A: "Neptune",
+            B: "Mercury",
+            C: "Uranus",
+            D: "Jupiter"
+        },
+        correctAnswer: "A"
+    },
+    {
+        question
+    }
+]
 
 function createQuiz(){
 

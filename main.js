b85b63f680fdf6fe99acb228c915a05d03bf44d3
Anova Hacks <anovahacks51@Anovas-MacBook-Pro.local>
2019-03-16 11:02:28 -0700

    update

--- main.js
+++ main.js
@@ -2,9 +2,3 @@ var value = 0;
 
 document.getElementById('score').innerText = ('Score: ' + value);
 
-document.getElementById('wrong').onclick = wrongAnswer;
-
-function wrongAnswer(){
-    document.getElementById('answer').innerText = "That's the wrong answer";
-
-}
